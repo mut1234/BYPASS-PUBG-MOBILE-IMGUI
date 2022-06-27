50b3d9e28d8825d0fefdcba5a7aa6c0c88678784
Mustafa Ghnaim <44782684+mut1234@users.noreply.github.com>
2022-06-28 00:36:35 +0300

    Add files via upload

---
+++ keyauth/auth.hpp
@@ -0,0 +1,60 @@
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+namespace KeyAuth {
+	class api {
+	public:
+
+		std::string name, ownerid, secret, version, url, sslPin;
+
+		api(std::string name, std::string ownerid, std::string secret, std::string version, std::string url, std::string sslPin) : name(name), ownerid(ownerid), secret(secret), version(version), url(url), sslPin(sslPin) {}
+
+		void ban();
+		void init();
+		void check();
+		void log(std::string msg);
+		void license(std::string key);
+		std::string var(std::string varid);
+		std::string webhook(std::string id, std::string params);
+		void setvar(std::string var, std::string vardata);
+		std::string getvar(std::string var);
+		bool checkblack();
+		void upgrade(std::string username, std::string key);
+		void login(std::string username, std::string password);
+		void web_login();
+		void button(std::string value);
+		std::vector<unsigned char> download(std::string fileid);
+		void regstr(std::string username, std::string password, std::string key);
+
+
+	
+		
+
+		class data_class {
+		public:
+			// app data
+			std::string numUsers;
+			std::string numOnlineUsers;
+			std::string numKeys;
+			std::string version;
+			std::string customerPanelLink;
+			// user data
+			std::string username;
+			std::string ip;
+			std::string hwid;
+			std::string createdate;
+			std::string lastlogin;
+			std::vector<std::string> subscriptions;
+			std::string expiry;
+			// response data
+			bool success;
+			std::string message;
+		};
+		data_class data;
+
+	private:
+		std::string sessionid, enckey;
+	};
+}
